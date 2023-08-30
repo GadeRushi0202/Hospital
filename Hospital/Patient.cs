dd5a3d13c061c35c209a06978415b4a8acc8df0b
GadeRushi0202 <gaderushi0202@gmail.com>
2023-08-30 09:00:22 -0700

    Add project files.

---
+++ Hospital/Patient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string P_Name { get; set; }
+        public int P_Bill { get; set; }
+        public string Hospital_Name { get; set; }
+    }
+    public class Hospital
+    {
+        private List<Patient> patientlist;
+
+        public Hospital()
+        {
+            patientlist = new List<Patient>()
+            {
+                new Patient() { Id=1, P_Name="RushiKesh", P_Bill=65000,Hospital_Name="Impulse Hospital"},
+                new Patient() { Id=2, P_Name="Vishal", P_Bill=75000,Hospital_Name="Impulse Hospital"},
+                new Patient() { Id=3, P_Name="Rajesh", P_Bill=85000,Hospital_Name="Impulse Hospital"}
+
+            };
+        }
+        public List<Patient> GetPatients()
+        {
+            return patientlist;
+        }
+
+        public Patient GetPatientById(int Id)
+        {
+            Patient patient   = new Patient();
+            foreach(Patient pat in patientlist)
+            {
+                if (pat.Id == Id)
+                {
+                    patient = pat;
+                }
+            }
+            return patient;
+        }
+        public void AddPatient(Patient p)
+        {
+            patientlist.Add(p);
+        }
+        public void UpadatePatient(Patient p)
+        {
+            foreach(Patient pat in patientlist)
+            {
+                if(pat.Id == p.Id)
+                {
+                    pat.P_Name = p.P_Name;
+                    pat.P_Bill = p.P_Bill;
+                    pat.Hospital_Name = p.Hospital_Name;
+                    break;
+                }
+            }
+        }
+        public void DeletePatient(int Id)
+        {
+            foreach(Patient p in patientlist)
+            {
+                if (p.Id == Id)
+                {
+                    patientlist.Remove(p);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Hospital/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = 0;
+            Hospital hospital = new Hospital();
+            do
+            {
+                Console.WriteLine("--------_________________---------");
+                Console.WriteLine("1.Patient List");
+                Console.WriteLine("2.Get Patient By Id");
+                Console.WriteLine("3.Add Patient");
+                Console.WriteLine("4.Update Patient");
+                Console.WriteLine("5.Deletr Patient");
+                Console.WriteLine("Select Your Option");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        List<Patient> list = hospital.GetPatients();
+                        Console.WriteLine("Id \t P_Name \t P_Bill \t Hospital_Name");
+                        foreach (Patient p in list)
+                        {
+                            Console.WriteLine($"{p.Id} \t {p.P_Name} \t {p.P_Bill} \t        {p.Hospital_Name}");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter The Patient Id");
+                        int Id = Convert.ToInt32(Console.ReadLine());
+                        Patient p1 = hospital.GetPatientById(Id);
+                        Console.WriteLine("Id \t P_Name \t P_Bill \t Hospital_Name");
+                        Console.WriteLine($"{p1.Id} \t {p1.P_Name} \t {p1.P_Bill} \t        {p1.Hospital_Name}");
+                        break; 
+                    case 3:
+                        Patient p2 = new Patient();
+                        Console.WriteLine("Enter The Patient Id");
+                        p2.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter The Patient Name");
+                        p2.P_Name =Console.ReadLine();
+                        Console.WriteLine("Enter The Patient Totla Bill");
+                        p2.P_Bill= Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter The Patient Hospital Name");
+                        p2.Hospital_Name = Console.ReadLine();
+                        hospital.AddPatient(p2);
+                        Console.WriteLine("Patient Saved");
+                        break;
+                    case 4:
+                        Patient p3 = new Patient();
+                        Console.WriteLine("Enter The Patient Id");
+                        p3.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter The Patient Name");
+                        p3.P_Name = Console.ReadLine();
+                        Console.WriteLine("Enter The Patient Total Bill");
+                        p3.P_Bill = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter The Patient Hospital Name");
+                        p3.Hospital_Name = Console.ReadLine();
+                        hospital.UpadatePatient(p3);
+                        Console.WriteLine("Patient Update");
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter The Patient Id");
+                        int Id2 = Convert.ToInt32(Console.ReadLine());
+                        hospital.DeletePatient(Id2);
+                        Console.WriteLine($"{Id2} Patient Deleted" );
+                        break;
+                }
+                Console.WriteLine("Press 1 for Continue");
+                no = Convert.ToInt32(Console.ReadLine());
+            } 
+            while (no != 0);
+
+        }
+    }
+}
